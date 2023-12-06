5cc08c68d8cc4bd812f714d1db6150c6cfac7fb1
jemraider21 <jemraider21@gmail.com>
2023-12-06 18:00:27 -0500

    Restructured the file system of the program. JS files compile in the same directory as its TS file

---
+++ src/main/backend/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(`__dirname: ${__dirname}`);
+
+const rootDirPath = path.join(__dirname, "../frontend/");
+console.log(`rootDirPath: ${rootDirPath}`);
+app.use(express.static(rootDirPath));
+
+const indexFilePath = path.join(rootDirPath, "index.html");
+app.get('/', (req, res) => {
+  res.sendFile(indexFilePath);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Press Ctrl+C to quit.`);
+});
