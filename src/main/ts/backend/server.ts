7804958e29ebdf0abb0242a73b1d60ab26349467
jemraider21 <jemraider21@gmail.com>
2023-12-06 07:06:41 -0500

    Attempted to load all of the static javascript files so the server can pick them up. Still issues with serving them

--- src/main/ts/backend/server.ts
+++ src/main/ts/backend/server.ts
@@ -5,14 +5,33 @@ import { fileURLToPath } from 'url';
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(__dirname);
 
 const rootDirPath = path.join(__dirname, "../../");
-console.log(`Root path: ${rootDirPath}`);
-app.use(express.static(rootDirPath));
+console.log(`rootDirPath: ${rootDirPath}`);
+//app.use(express.static(rootDirPath));
+
+const jsFilePaths = new Map<string, string>();
+const jsFilesDirectory = "js/frontend";
+const jsFilePath = path.join(rootDirPath, jsFilesDirectory);
+jsFilePaths.set(jsFilesDirectory, jsFilePath);
+
+const jsUtilsDirectory = `${jsFilesDirectory}/utils`;
+const jsUtilsPath = path.join(rootDirPath, jsUtilsDirectory);
+jsFilePaths.set(jsUtilsDirectory, jsUtilsPath);
+
+["assets", "shapes", "structure"].forEach((dir) => {
+  const jsModelDirectory = `${jsFilesDirectory}/model/${dir}`;
+  const jsModelPath = path.join(rootDirPath, jsModelDirectory);
+  jsFilePaths.set(jsModelDirectory, jsModelPath);
+});
+
+jsFilePaths.forEach((value, key) => {
+  const url = "/" + key;
+  console.log(`${url}: ${value}`);
+  app.use(url, express.static(value));
+});
 
 const indexFilePath = path.join(rootDirPath, "index.html");
-console.log(`Index file path: ${indexFilePath}`);
 app.get('/', (req, res) => {
   res.sendFile(indexFilePath);
 });
