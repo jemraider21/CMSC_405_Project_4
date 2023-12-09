22a43c21e4e8c4da486b73f4ef1cdd86ffc0492c
jemraider21 <jemraider21@gmail.com>
2023-12-08 20:19:43 -0500

    Formatted colors for the server text output

--- src/main/backend/server.js
+++ src/main/backend/server.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import chalk from 'chalk';
+console.log(chalk.whiteBright("\n\nSetting up server to service the program..."));
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(`__dirname: ${__dirname}`);
 const rootDirPath = path.join(__dirname, "../frontend/");
-console.log(`rootDirPath: ${rootDirPath}`);
 app.use(express.static(rootDirPath));
 const indexFilePath = path.join(rootDirPath, "index.html");
 app.get('/', (req, res) => {
     res.sendFile(indexFilePath);
 });
 app.listen(port, () => {
-    console.log("\x1b[33m", `Server running at http://localhost:${port}`);
-    console.log(`Press Ctrl+C to quit.`);
+    console.log(chalk.green(`Server started at http://localhost:${port}`));
+    console.log(chalk.cyan(`Press Ctrl+C to quit.`));
 });
--- src/main/backend/server.ts
+++ src/main/backend/server.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import chalk from 'chalk';
 
+console.log(chalk.whiteBright("\n\nSetting up server to service the program..."));
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(`__dirname: ${__dirname}`);
 
 const rootDirPath = path.join(__dirname, "../frontend/");
-console.log(`rootDirPath: ${rootDirPath}`);
 app.use(express.static(rootDirPath));
 
 const indexFilePath = path.join(rootDirPath, "index.html");
@@ -17,6 +17,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("\x1b[33m", `Server running at http://localhost:${port}`);
-  console.log(`Press Ctrl+C to quit.`);
+  console.log(chalk.green(`Server started at http://localhost:${port}`));
+  console.log(chalk.cyan(`Press Ctrl+C to quit.`));
 });
